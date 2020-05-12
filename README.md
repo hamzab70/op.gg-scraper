a2a460e710adbc44b43dd650f857c6978eee8595
hamzab70 <hboulaich23@gmail.com>
2020-05-12 19:59:12 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# op.gg-SoloQ-Challegenge-Scraper
+# op.gg Scraper
+
+Web scrapper that uses op.gg and League Of Legends API to sort a list of players using the same format as https://soloqchallenge.gg/
+
+
+
+## Requirements
+
+You can install all the dependencies using pip3 and the requirements.txt, just execute:
+
+```
+pip3 install -r requirements.txt 
+```
+
+
+
+## Usage
+
+Clone the repo:
+
+```
+git clone https://github.com/hamzab70/op.gg-Scraper.git
+```
+
+Open opgg.py and change this 3 variables:
+
+* server = the accounts server
+* players_num = the total number of players you want to scrape
+* players = is a list of dictionaries that include the name of each player and his LOL account, so you must create a new entry for each player you want to scrape
+
+Then you can execute the scrapper:
+
+```
+python3 opgg.py
+```
+
+and the result should be something like:
+
+```
+=====  =========  ===============  ==========  ======  =======  ======  ========  =========
+  Pos  Player     Account          Elo         LP        Games    Wins    Losses  Winrate
+=====  =========  ===============  ==========  ======  =======  ======  ========  =========
+    1  Nino       TROPHY WINNER    Platinum 4  0 LP         70      48        22  69%
+    2  Ivan       MC Vergote       Gold 1      0 LP         60      39        21  65%
+    3  Shaggy     LC Spanish Uzi   Gold 1      0 LP         72      40        32  56%
+    4  Miguel     El Pinche Joto   Gold 3      59 LP        80      48        32  60%
+    5  Eric       Blackbird SR71   Silver 1    38 LP        69      36        33  52%
+    6  Guaye      RobertazpeLkN    Silver 4    100 LP       47      22        25  47%
+    7  Igna       MarthaeSoSo      Silver 4    69 LP        33      17        16  52%
+    8  Parejo     RITOPLSDONTBAN   Bronze 1    0 LP         15       7         8  47%
+    9  Sergi      Papingo Ibaka    Bronze 3    85 LP        16       7         9  44%
+   10  Hamza      Kerry Co Boalak  Bronze 3    3 LP         10       4         6  40%
+   11  Pol        Topacio Tenorio  Bronze 4    86 LP        12       4         8  33%
+   12  Bastardas  r u fine         Unranked    0 LP          0       0         0  0%
+=====  =========  ===============  ==========  ======  =======  ======  ========  =========
+```
+
+
+
+## Known issues
+
+- It doesn't get along very well with unranked accounts, so if a player is unranked it just prints empty data.
+- The sorting is done by sort(a,b) based in the League > LeaguePoints > Winratio > Wins, if two or more players have the same data in this four variables it just sort them by alphabetical order.
--- opgg.py
+++ opgg.py
@@ -4,12 +4,14 @@ from bs4 import *
 from tabulate import tabulate
 
 ############################ VARIABLES ########################################
+server = "euw"
 players_num = 12
-players = [{"name": "Eric","cuenta": "Blackbird+SR71"},{"name": "Nino","cuenta":"TROPHY+WINNER"},{"name": "Igna","cuenta": "MarthaeSoSo"},
-		   {"name": "Miguel","cuenta": "El+Pinche+Joto"},{"name": "Hamza","cuenta": "Kerry+Co+Boalak"},{"name": "Bastardas","cuenta": "r+u+fine"},
-		   {"name": "Pol","cuenta": "Topacio+Tenorio"},{"name": "Shaggy","cuenta": "LC+Spanish+Uzi"},{"name": "Ivan","cuenta": "MC+Vergote"},
-		   {"name": "Guaye","cuenta": "RobertazpeLkN"},{"name": "Parejo","cuenta": "RITOPLSDONTBAN"},{"name": "Sergi","cuenta": "Papingo+Ibaka"}]
+players = [{"name": "Eric","account": "Blackbird SR71"},{"name": "Nino","account":"TROPHY WINNER"},{"name": "Igna","account": "MarthaeSoSo"},
+	   {"name": "Miguel","account": "El Pinche Joto"},{"name": "Hamza","account": "Kerry Co Boalak"},{"name": "Bastardas","account": "r u fine"},
+	   {"name": "Pol","account": "Topacio Tenorio"},{"name": "Shaggy","account": "LC Spanish Uzi"},{"name": "Ivan","account": "MC Vergote"},
+	   {"name": "Guaye","account": "RobertazpeLkN"},{"name": "Parejo","account": "RITOPLSDONTBAN"},{"name": "Sergi","account": "Papingo Ibaka"}]
 ################################################################################
+
 ranks = ["Challenger","Grandmaster","Master","Diamond 1","Diamond 2","Diamond 3","Diamond 4","Platinum 1","Platinum 2","Platinum 3","Platinum 4","Gold 1","Gold 2","Gold 3","Gold 4","Silver 1","Silver 2","Silver 3","Silver 4","Bronze 1","Bronze 2","Bronze 3","Bronze 4","Iron 1","Iron 2","Iron 3", "Iron 4","Unranked"]
 place = list(range(1,players_num+1))
 printable_list = []
@@ -17,11 +19,11 @@ printable_list = []
 session = HTMLSession()
 
 for name in players:
-	
-	player = name["name"]
-	name = name["cuenta"]
 
-	page = 'https://euw.op.gg/summoner/userName=' + name
+	player = name["name"]
+	account = name["account"].replace(" ","+")
+
+	page = 'https://' + server + '.op.gg/summoner/userName=' + account
 	html = session.get(page)
 	soup = BeautifulSoup(html.content, 'html.parser')
 
@@ -36,7 +38,7 @@ for name in players:
 	wins = soup.find("span", {"class": "wins"})
 	if wins is not None:
 		wins = wins.string
-		wins = wins.replace("W","") 
+		wins = wins.replace("W","")
 		wins = int(wins)
 	else:
 		wins = 0
@@ -44,7 +46,7 @@ for name in players:
 	losses = soup.find("span", {"class": "losses"})
 	if losses is not None:
 		losses = losses.string
-		losses = losses.replace("L","") 
+		losses = losses.replace("L","")
 		losses = int(losses)
 	else:
 		losses = 0
@@ -52,25 +54,25 @@ for name in players:
 	winratio = soup.find("span", {"class": "winratio"})
 	if winratio is not None:
 		winratio = winratio.string
-		winratio = winratio.replace("Win Ratio ","") 
+		winratio = winratio.replace("Win Ratio ","")
 	else:
-		winratio = "//%"
+		winratio = "0%"
 
 	if LP is not None:
-		liga = rank
+		league = rank
 	else:
-		liga = "Unranked"
+		league = "Unranked"
 
 	if wins is not None:
 		games = wins+losses
 
-	name = name.replace("+"," ")
+	name = name["account"]
 
-	printable_list.append({"player": player ,"name": name, "liga": liga, "LP": LP, "games": games, "wins": wins, "losses": losses, "winratio": winratio})
+	printable_list.append({"player": player ,"name": name, "league": league, "LP": LP, "games": games, "wins": wins, "losses": losses, "winratio": winratio})
 
 def sort(a,b):
-	if a["liga"] == b["liga"]:
-		if int(a["LP"].split(" ")[0]) > int(b["LP"].split(" ")[0]): 
+	if a["league"] == b["league"]:
+		if int(a["LP"].split(" ")[0]) > int(b["LP"].split(" ")[0]):
 			return -1
 		elif int(a["LP"].split(" ")[0]) < int(b["LP"].split(" ")[0]):
 			return 1
@@ -80,14 +82,14 @@ def sort(a,b):
 			if a["wins"] > b["wins"]:
 				return -1
 			else:
-				return 1	
+				return 1
 	else:
-		if ranks.index(a["liga"]) > ranks.index(b["liga"]):
+		if ranks.index(a["league"]) > ranks.index(b["league"]):
 			return 1
 		else:
 			return -1
-			
+
 cmp = functools.cmp_to_key(sort)
 printable_list.sort(key=cmp)
 
-print(tabulate([elem.values() for elem in printable_list], headers=['Pos','Nombre', 'Cuenta', 'Elo', 'LP', 'Partidas', 'Wins', "Losses", "Winrate"], showindex=place, tablefmt="rst", numalign="right", stralign="left"))
+print(tabulate([elem.values() for elem in printable_list], headers=['Pos','Player', 'Account', 'Elo', 'LP', 'Games', 'Wins', "Losses", "Winrate"], showindex=place, tablefmt="rst", numalign="right", stralign="left"))
