ebf70c392eaab61bf6043395d56c77651557db84
Nick Wang <nwang0719@gmail.com>
2023-10-17 02:51:35 -0400

    Functionized code

--- opgg.py
+++ opgg.py
@@ -2,19 +2,17 @@ import functools
 from requests_html import HTMLSession
 from bs4 import *
 from tabulate import tabulate
-import pprint
 
-# https://www.op.gg/multisearch/na?summoners=Crusading%20Dino%2C%20Hdannihilator%2C%20Arri%2C%20xGreySwag%2C%20Defaults01%2C%20DryEyesWhiteDrag%2C%20MAgicdragon0987%2C%20AlexGdawg
 ############################ VARIABLES ########################################
 server = "na"
 players = [{"name": "Devon","account": "Hdannihilator"},
-		   {"name": "Alex G","account":"Crusading Dino"}, 
+		   {"name": "Alex M","account":"Crusading Dino"}, 
 		   {"name": "Lamont","account":"Arri"},
 		   {"name": "Grayson","account":"xGreySwag"},
 		   {"name": "Alberto","account":"Defaults01"},
 		   {"name": "Matthew","account":"DryEyesWhiteDrag"},
 		   {"name": "Ethan","account":"MAgicdragon0987"},
-		   {"name": "Alex M","account":"AlexGdawg"}]
+		   {"name": "Alex G","account":"AlexGdawg"}]
 ################################################################################
 ranks = ["challenger","grandmaster","master",
 		 "diamond1","diamond2","diamond3","diamond4",
@@ -26,70 +24,75 @@ ranks = ["challenger","grandmaster","master",
 		 "iron1","iron2","iron3", "iron4",
 		 "unranked"]
 
+def main():
 
-printable_list = []
+	printable_list = []
 
-session = HTMLSession()
-
-for name in players:
-
-	page = 'https://' + server + '.op.gg/summoner/userName=' + name["account"].replace(" ","+")
-	html = session.get(page)
-	soup = BeautifulSoup(html.content, 'html.parser')
+	session = HTMLSession()
 
 
-	htmll = soup.prettify()  #bs is your BeautifulSoup object
+	for name in players:
 
-
-	try:
-		rank = soup.find("div", {"class": "tier"}).getText(strip=True)
+		page = 'https://' + server + '.op.gg/summoner/userName=' + name["account"].replace(" ","+")
+		html = session.get(page)
+		soup = BeautifulSoup(html.content, 'html.parser')
 		
-	except:
-		rank = "unranked"
-
-	LP = soup.find("div", {"class": "lp"})
-	if LP is not None:
-		LP = LP.getText(strip=True)
-	else:
-		LP = "0 LP"
-
-	wins_losses = soup.find("div", {"class": "win-lose"})
-	if wins_losses is not None:
-		# wins = int(wins.getText(strip=True).replace("W",""))
-
-		wins, losses = wins_losses.getText(strip=True).replace("W", " ").replace("L", "").split()
-	else:
-		wins, losses = 0, 0
-
-	if int(wins) + int(losses) == 0:
-		winratio = "0%"
-	else:
-		winratio = str(round((int(wins) / (int(wins) + int(losses))) * 100, 2)) + "%"
-		
+		# htmll = soup.prettify()  #bs is your BeautifulSoup object
+		# with open('./{}.txt'.format('out'), mode='wt', encoding='utf-8') as file:
+		# 	file.write(htmll)
 
 
-	if LP is not None:
-		league = rank
-	else:
-		league = "Unranked"
+		try:
+			rank = soup.find("div", {"class": "tier"}).getText(strip=True)
+			
+		except:
+			rank = "unranked"
 
-	if wins is not None:
-		games = int(wins)+int(losses)
+		LP = soup.find("div", {"class": "lp"})
+		if LP is not None:
+			LP = LP.getText(strip=True)
+		else:
+			LP = "0 LP"
 
-	promo = ""
-	if LP == "100 LP":
-		promo = str(soup.find("ol", {"class": "SeriesResults"}))
+		wins_losses = soup.find("div", {"class": "win-lose"})
+		if wins_losses is not None:
+			# wins = int(wins.getText(strip=True).replace("W",""))
 
-		promow = promo.count('__spSite __spSite-156')
-		promol = promo.count('__spSite __spSite-154')
+			wins, losses = wins_losses.getText(strip=True).replace("W", " ").replace("L", "").split()
+		else:
+			wins, losses = 0, 0
 
-		promo = str(promow) + 'W-' + str(promol) + 'L'
+		if int(wins) + int(losses) == 0:
+			winratio = "0%"
+		else:
+			winratio = str(round((int(wins) / (int(wins) + int(losses))) * 100, 2)) + "%"
+			
 
-	printable_list.append({"pos": 0, "player": name["name"] ,"name": name["account"], "league": league, "LP": LP, "promo": promo, "games": games, "wins": wins, "losses": losses, "winratio": winratio})
 
+		if LP is not None:
+			league = rank
+		else:
+			league = "Unranked"
+
+		if wins is not None:
+			games = int(wins)+int(losses)
+
+		promo = ""
+		if LP == "100 LP":
+			promo = str(soup.find("ol", {"class": "SeriesResults"}))
+
+			promow = promo.count('__spSite __spSite-156')
+			promol = promo.count('__spSite __spSite-154')
+
+			promo = str(promow) + 'W-' + str(promol) + 'L'
+
+		printable_list.append({"pos": 0, "player": name["name"] ,"name": name["account"], "league": league, "LP": LP, "promo": promo, "games": games, "wins": wins, "losses": losses, "winratio": winratio})
+	
+	print_list(printable_list)
+
+### SORT PLAYERS BY RANK FOR PRINTING ###
 def sort(a,b):
 	if a["league"] == b["league"]:
-		# if int(a["LP"].split(" ")[0]) > int(b["LP"].split(" ")[0]):
 		if int(a["LP"][:-2]) > int(b["LP"][:-2]):
 			return -1
 		elif int(int(a["LP"][:-2])) < int(b["LP"][:-2]):
@@ -107,20 +110,28 @@ def sort(a,b):
 		else:
 			return -1
 
-cmp = functools.cmp_to_key(sort)
-printable_list.sort(key=cmp)
+### FORMAT AND PRINT LIST ###
+def print_list(printable_list):
 
-pos = 0
-for player in printable_list:
-	pos += 1
-	player["pos"] = pos
-	if player['LP'] == "100 LP":
-		player["league"] = player["league"] + ' (' + player["promo"] + ')'
-		del player['LP']
-		del player['promo']
-	else:
-		player["league"] = player["league"] + ' (' + player["LP"] + ')'
-		del player['LP']
-		del player['promo']
+	cmp = functools.cmp_to_key(sort)
+	printable_list.sort(key=cmp)
 
-print(tabulate([elem.values() for elem in printable_list], headers=['Pos','Player', 'Account', 'Elo', 'Games', 'Wins', "Losses", "Winrate"], tablefmt="rst", numalign="right", stralign="left"))
+	pos = 0
+	for player in printable_list:
+		pos += 1
+		player["pos"] = pos
+		if player['LP'] == "100 LP":
+			player["league"] = player["league"] + ' (' + player["promo"] + ')'
+			del player['LP']
+			del player['promo']
+		else:
+			player["league"] = player["league"] + ' (' + player["LP"] + ')'
+			del player['LP']
+			del player['promo']
+
+	print(tabulate([elem.values() for elem in printable_list], headers=['Pos','Player', 'Account', 'Elo', 'Games', 'Wins', "Losses", "Winrate"], tablefmt="rst", numalign="right", stralign="left"))
+
+
+
+if __name__ == "__main__":
+	main()
